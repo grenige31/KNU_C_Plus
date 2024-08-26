9b8f6144b965d49bfd2644ac3cd8d444447d6e94
grenige31 <rengie31@gmail.com>
2024-08-26 14:35:34 +0900

    ch02 lab1.c upload

---
+++ source/ch02/ch02_1_lab1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int math = 100;
+	int kor = 90;
+	int music = 80;
+	int eng = 90;
+	printf("수학 : %d\n", math);
+	printf("국어 : %d\n", kor); 
+	printf("음악 : %d\n", music);
+	printf("영어 : %d\n", eng);
+	return 0;
+}
