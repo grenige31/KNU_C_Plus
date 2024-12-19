abf2373dcc27adea6e12dc8882c9601d26d7691a
grenige31 <rengie31@gmail.com>
2024-12-19 13:02:14 +0900

    sh copy

--- source/ch11/ex5-10-ShallowCopy.cpp
+++ source/ch11/ex5-10-ShallowCopy.cpp
@@ -1,48 +1,51 @@
-#define _CRT_SECURE_NO_WARNINGS //비주얼 스튜디오에서 strcpy로 인한 오류를 막기 위한 선언문
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-class Person { // Person 클래스 선언
-	char* name;
-	int id;
+class ShallowCopy {
+private:
+    int* arr; // 배열을 동적 할당으로 관리
+    int size;
 public:
-	Person(int id, const char* name); // 생성자
-	~Person(); // 소멸자
-	void changeName(const char *name);
-	void show() { cout << id << ',' << name << endl; }
+    // 생성자
+    ShallowCopy(int s)  {
+        arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i + 1; // 배열 초기화
+        }
+    }
+    // 복사 생성자 (shallow copy 복사)
+    ShallowCopy(const ShallowCopy& obj) {
+        arr = obj.arr; // 메모리 주소만 복사
+        size = obj.size;
+    }
+
+    void setElement(int index, int value) {
+        if (index >= 0 && index < size) {
+            arr[index] = value;
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++) cout << arr[i] << " ";
+        cout << endl;
+    }
+
+    ~ShallowCopy() {
+        delete[] arr; // 메모리 해제
+    }
 };
 
-Person::Person(int id,const char* name) { // 생성자
-	this->id = id;
-	int len = strlen(name); // name의 문자 개수
-	this->name = new char [len+1]; // name 문자열 공간 핟당
-	strcpy(this->name, name); // name에 문자열 복사
-}
-
-Person::~Person() {// 소멸자
-	if(name) // 만일 name에 동적 할당된 배열이 있으면
-		delete [] name; // 동적 할당 메모리 소멸
-}
-
-void Person::changeName(const char* name) { // 이름 변경
-	if(strlen(name) > strlen(this->name))
-		return; // 현재 name에 할당된 메모리보다 긴 이름으로 바꿀 수 없다.
-	strcpy(this->name, name);
-}
-
 int main() {
-	Person father(1, "Kitae");			// (1) father 객체 생성
-	Person daughter(father);			// (2) daughter 객체 복사 생성. 복사 생성자 호출
+    ShallowCopy obj1(5);
+    ShallowCopy obj2 = obj1; // 얕은 복사
 
-	cout << "daughter 객체 생성 직후 ----" << endl;
-	father.show();						// (3) father 객체 출력
-	daughter.show();					// (3) daughter 객체 출력
+    cout << "Original array in obj1: ";
+    obj1.print();
 
-	daughter.changeName("Grace");		// (4) 	daughter의 이름을 "Grace"로 변경
-	cout << "daughter 이름을 Grace로 변경한 후 ----" << endl;
-	father.show();						// (5) father 객체 출력
-	daughter.show();					// (5) daughter 객체 출력
+    obj2.setElement(0, 100); // obj2에서 값 변경
 
-	return 0;							// (6), (7) daughter, father 객체 소멸
+    cout << "Modified array in obj1 (affected by obj2): ";
+    obj1.print(); // obj1도 영향을 받음
+
+    return 0;
 }
